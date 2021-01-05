bb055eafa7458dc400c547c94226f1e5a620be83
mC3Lmu <72661100+muelver0002@users.noreply.github.com>
2021-01-06 06:06:31 +0800

    Added Student Class

---
+++ SharpDevelopWebApi/Models/Student.cs
@@ -0,0 +1,45 @@
+﻿
+using System;
+
+namespace SharpDevelopWebApi.Models
+{
+	/// <summary>
+	/// Description of Student.
+	/// </summary>
+	public class Student
+	{
+		
+		public int Id {get; set;}
+		public string FirstName {get; set;}
+		public string LastName {get; set;}
+		public string MiddleName {get; set;}
+		public DateTime? BirthDate {get; set;}
+		public string Address {get; set;}
+		public string Gradelevl {get; set;}
+		public DateTime? DateEnrollment {get;set;}
+		
+		public string FathersName {get; set;}
+		public string MothersName {get;set;}
+		public string GuardiansName {get; set;}
+		public string Contact {get; set;}
+		
+		public string LastSchoolAttended {get; set;}
+		public string SchoolId {get; set;}
+		
+			
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
